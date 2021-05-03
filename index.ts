9a987d7ebdf8b2fc93ecd513c2d94ddcd4996a63
GrzegorzNecka <strzypior@gmail.com>
2021-05-03 15:19:59 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,32 +1,35 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 class DanePogodowe {
-  constructor(public title: string) {
-    this.title = title
+  constructor(
+    public temperatura: string,
+    public wilgotnosc: string,
+    public cisnienie: string
+  ) {
+    this.temperatura = temperatura;
+    this.wilgotnosc = wilgotnosc;
+    this.cisnienie = cisnienie;
   }
 
-
-    pobierzTemperature(): string {
-    return this.temperatur
+  pobierzTemperature(): string {
+    return this.temperatura;
   }
-     pobierzWilgotnosc(): string {
-    return this.wilgotnosc
+  pobierzWilgotnosc(): string {
+    return this.wilgotnosc;
   }
-     pobierzCisnienie(): string {
-    return this.temperatur
-  }
-
-       odczytZamiana(): string {
-    return this.temperatur
+  pobierzCisnienie(): string {
+    return this.cisnienie;
   }
 
+  odczytZamiana(): void {
+    console.log("abc");
+  }
 }
 
+// let message: string = "I know your password, bro!";
+// let show = (message: string): void => {
+//   console.log(message);
+// };
 
-let message: string = "I know your password, bro!"
-let show = (message: string): void => {
- console.log(message)
-}
-
-show(message) /*?*/ //undefined
+// show(message); /*?*/ //undefined
