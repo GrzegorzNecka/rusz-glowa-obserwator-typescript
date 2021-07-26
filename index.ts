95990f2506eb4d028c33673c59788b4769ad1b03
GrzegorzNecka <strzypior@gmail.com>
2021-07-26 22:59:43 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,31 +1,40 @@
 // Import stylesheets
-import "./style.css";
+// import './style.css';
 
-class DanePogodowe {
-  constructor(
-    public temperatura: string,
-    public wilgotnosc: string,
-    public cisnienie: string
-  ) {
-    this.temperatura = temperatura;
-    this.wilgotnosc = wilgotnosc;
-    this.cisnienie = cisnienie;
-  }
-
-  pobierzTemperature(): string {
-    return this.temperatura;
-  }
-  pobierzWilgotnosc(): string {
-    return this.wilgotnosc;
-  }
-  pobierzCisnienie(): string {
-    return this.cisnienie;
-  }
-
-  odczytZamiana(): void {
-    console.log("abc");
-  }
+interface Obserwator {
+  aktualizacja(temp: number, wilgotnosc: number, cisnienie: number): void;
 }
+
+interface Podmiot {
+  zarejestrujObserwatora({ obj: Obserwator }): void;
+  usunObserwatora({ obj: Obserwator }): void;
+}
+
+// class DanePogodowe implements Podmiot {
+//   constructor(
+//     public temperatura: number,
+//     public wilgotnosc: number,
+//     public cisnienie: number
+//   ) {
+//     this.temperatura = temperatura;
+//     this.wilgotnosc = wilgotnosc;
+//     this.cisnienie = cisnienie;
+//   }
+
+//   pobierzTemperature(): string {
+//     return this.temperatura;
+//   }
+//   pobierzWilgotnosc(): string {
+//     return this.wilgotnosc;
+//   }
+//   pobierzCisnienie(): string {
+//     return this.cisnienie;
+//   }
+
+//   odczytZamiana(): void {
+//     console.log('abc');
+//   }
+// }
 
 // let show = (message: string): void => {
 //   console.log(message);
